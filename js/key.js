ab2027f2da9fad744b401a395921f8d63bcbe6fe
poberherr <patrick.oberherr@gmail.com>
2012-09-23 20:32:01 +0200

    added simple meshmorphing and played around with some input stuff

--- js/key.js
+++ js/key.js
@@ -1,39 +1,87 @@
-var direction = 's';
+var direction = 'd';
+var prev_action = false;
+mouseX = 0;
+mouseY = 0;
+winW = window.innerWidth;
+winH = window.innerHeight;
+player_x=winW/2;
+player_y=winH/2;
+
+$(document).mousemove(function(e){
+	      /*var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
+		  var clientCoords = "( " + e.clientX + ", " + e.clientY + " )";
+			alert(pageCoords + "," + clientCoords);*/
+	mouseX = e.pageX;
+	mouseY = e.pageY;
+
+
+});
+
+$(document).onclick(function(e){
+
+	// TODO: Do magic here 
+
+});
+
 function keyboardEvents(){
 
+	action = false;
 	if( tQuery.keyboard().pressed('w') )  {
 		if(direction != 'w'){
-			bla.rotation(0,3,0);
+			dae.rotation.y = 1.5;
+		}else{
+			bla.translateZ(-0.1);
 		}
-		bla.translateZ(-0.1);
 		direction = 'w';
+		action = true;
 	}
 	if( tQuery.keyboard().pressed('a') ) {
 		if(direction != 'a'){
-			bla.rotation(0,4.5,0);
+			dae.rotation.y = 3.3 ;
+		}else{
+			bla.translateX(-0.1);	
 		}
-		bla.translateX(-0.1);	
 		direction = 'a';
+		action = true;
 	}
 	if( tQuery.keyboard().pressed('s') ) {
 		if(direction != 's'){
-			bla.rotation(0,0,0);
+			dae.rotation.y = -1.5;
+		}else{
+			bla.translateZ(0.1);	
 		}
-		bla.translateZ(0.1);	
 		direction = 's';
+		action = true;
 	}
 	if( tQuery.keyboard().pressed('d') ) {
 		if(direction != 'd'){
-			bla.rotation(0,1.5,0);
+			dae.rotation.y = 0;
+		}else{
+			bla.translateX(0.1);
 		}
-		bla.translateX(0.1);
 		direction = 'd';
+		action = true;
+	}
+
+	if( tQuery.keyboard().pressed('space') ) {
+		worldrotx+=0.1;
+		console.log(worldrotx);
 	}	
 
-	if( tQuery.keyboard().pressed('right') ) {
-		cangle++;//world.camera().position.x++;	
+	if( tQuery.keyboard().pressed('up') ) {
+		bla.translateY(0.1);
+		action = true;
+	}	
+	if( tQuery.keyboard().pressed('down') ) {
+		bla.translateY(-0.1);
+		action = true;
 	}
-	if( tQuery.keyboard().pressed('left') ) {
-		cangle--;//world.camera().position.x--;
+	prev_action = action;
+
+	if( !prev_action && !action){
+		runani = false;
+	}else{
+		runani = true;
 	}
+
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+	var runani=true;
+	var dae = null;
+	var worldrotx = 0.0;
 
 $(document).ready(function(){
 
@@ -40,17 +43,34 @@ $(document).ready(function(){
 
 	
 	/* Create World with tquery */
-	var world	= tQuery.createWorld().boilerplate().start();
+	world	= tQuery.createWorld().boilerplate().start();
 
 
 	// remove camera controls to be easily clickable
 	world.removeCameraControls();
 
-
+	/*
 	world.camera().position.x = 0;
  	world.camera().position.y = 2;	
  	world.camera().position.z = 10;	
-		
+	*/	
+
+
+/**
+ * Returns a random number between min and max
+  */
+  function getRandomArbitary (min, max) {
+	      return Math.random() * (max - min) + min;
+  }
+
+  /**
+   * Returns a random integer between min and max
+    * Using Math.round() will give you a non-uniform distribution!
+	 */
+	 function getRandomInt (min, max) {
+		     return Math.floor(Math.random() * (max - min + 1)) + min;
+	 }
+
 
 	tQuery.registerStatic('createPlanet', function(opts){
 		var url		= 'img/world.png';
@@ -63,19 +83,36 @@ $(document).ready(function(){
 		return object;
 	});
 
-	var planet3	= tQuery.createPlanet().translateY(1).addTo(world);
+	for(i=0;i<10;i++){
+		var x = getRandomInt(-10,10); 
+		var y = getRandomInt(  0,10); 
+		var z = getRandomInt(-10,10);
+		var s = getRandomArbitary(0.1,2); 
+		var blub = getRandomArbitary(1,10);
+		tQuery.createPlanet().scale(s).translateX(x).translateY(y).translateZ(z).addTo(world);
+
+		tQuery.createCheckerboard({
+			segmentsW	: 10,
+			segmentsH	: 10
+		}).rotate(x,y,z).translateX(x).translateY(y).translateZ(z).addTo(world).scaleBy(blub);
+
+	}
 
 	tQuery.createCheckerboard({
-		segmentsW	: 10,
-		segmentsH	: 10
-	}).addTo(world).scaleBy(10);
+		segmentsW	: 40,
+		segmentsH	: 40
+	}).addTo(world).scaleBy(40);
 
 
 	var loader = new THREE.ColladaLoader();
+	var skin = null;
 	loader.options.convertUpAxis = true;
 	loader.load( 'model/monster/monster.dae', function ( collada ) {
-				var dae = collada.scene;
-				var skin = collada.skins[ 0 ];
+				dae = collada.scene;
+				skin = collada.skins[ 0 ];
+
+			/*	console.log("col: list animmation", Object.keys(collada.animations));
+				console.log("dae: list animmation", Object.keys(collada.dae.animations)); */
 
 				bla  = tQuery(dae).scale(0.0005).translateX(-0.3).addTo(world);
 
@@ -83,12 +120,18 @@ $(document).ready(function(){
 					keyboardEvents();
 
 					var np = dae.position;
+							 //console.log("rot:" , dae.rotation);
+
+					var h = 15;
+					var g = Math.sin(worldrotx)*h;
+					var a = Math.cos(worldrotx)*h;
 					updateCamera(	world.tCamera(),
-						[np.x, np.y + 1 ,np.z + 5],
-						[np.x, np.y     ,np.z    ]
+						[np.x + g, np.y + 5 ,np.z + a],
+						[np.x , np.y     ,np.z    ]
 					);
 				})
 
+				animate();
 
 	 });
 
@@ -97,6 +140,37 @@ $(document).ready(function(){
 	tQuery.createDirectionalLight().addTo(world).position(10,10,3);
 	tQuery.createAmbientLight().addTo(world).color(0xFFFFFF);
 
+	var t = 0;
+	var clock = new THREE.Clock();
 
+
+	function animate() {
+
+		var delta = clock.getDelta();
+
+		requestAnimationFrame( animate );
+
+		if ( t > 1 ) t = 0;
+
+		if ( skin ) {
+
+
+			if( runani ){
+				for ( var i = 0; i < skin.morphTargetInfluences.length; i++ ) {
+
+					skin.morphTargetInfluences[ i ] = 0;
+
+				}
+
+				skin.morphTargetInfluences[ Math.floor( t * 30 ) ] = 1;
+
+				t += delta;
+			}
+
+
+		}
+
+
+	}
 
 });	
