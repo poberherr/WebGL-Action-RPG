c6610346786ce4ffe1a531fba4228d772c7f4760
poberherr <patrick.oberherr@gmail.com>
2012-09-23 06:14:33 +0200

    last cleanup befor epic sleeptime

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 $html  = '';
 
-
-/*
- *  Might change ... just do it 
- */
-
-$extJSDir="external/js/";
-$extCSSDir="external/css/";
+$extdir="external/";
+$extJSDir=$extdir."js/";
+$extCSSDir=$extdir."css/";
 
 $jsDir="js/";
 $cssDir="css/";
@@ -40,76 +36,48 @@ $intJS= array(
 		'main',
 );
 
-
 /**
- *  Dont touch stuff below this line :) pretty please 
+ *  Dont touch stuff below this line :) pretty please ~ tut sowieso keiner -_- den Satz haett ich mir sparen koennen ~ inbre
  **/ 
 
 $html .= '<html>';
 $html .= '<head/>';
 
-
 /*
  *  Abgelenkt ah...ja so nennt man das also heute :-p 
  */ 
-
-foreach ($extCSS as $cssFileName)
-{
-	$html .= '<link type="text/css" href="';
-	$html .= $extCSSDir.$cssFileName.".css";
-	$html .= '" rel="stylesheet" />'."\n";
+foreach ($extCSS as $cssFileName){
+	$html .= '<link type="text/css" href="'.$extCSSDir.$cssFileName.'.css" rel="stylesheet" />'."\n";
 }
-
-foreach ($intCSS as $cssFileName)
-{
-	$html .= '<link type="text/css" href="';
-	$html .= $cssDir.$cssFileName.".css";
-	$html .= '" rel="stylesheet" />';
+foreach ($intCSS as $cssFileName){
+	$html .= '<link type="text/css" href="'.$cssDir.$cssFileName.'.css" rel="stylesheet" />'."\n";
 }
-
-foreach ($extJS as $jsFileName)
-{
-	$html .= '<script ';
-	$html .= 'src="';
-	$html .= $extJSDir.$jsFileName.".js";
-	$html .= '"';
-	$html .= ' ></script>'."\n";
+foreach ($extJS as $jsFileName){
+	$html .= '<script src="'.$extJSDir.$jsFileName.'.js" ></script>'."\n";
 }
-
-foreach ($intJS as $jsFileName)
-{
-	$html .= '<script ';
-	$html .= 'src="';
-	$html .= $jsDir.$jsFileName.".js";
-	//$html .= "all.js";
-	$html .= '"';
-	$html .= ' ></script>'."\n";
+foreach ($intJS as $jsFileName){
+	$html .= '<script src="'.$jsDir.$jsFileName.'.js" ></script>'."\n";
 }
-
-$html .= '
-<style>
-#toolbar {
-	padding: 20px 6px;
-}
-</style>';
 
 $html .= '<body>';
 
+/*
+ * TODO:  P.O: - Raem doch endlich mla dein Zimmer auf aber zack zack ... das ist ja die hoelle in farbe und 3D  ~ inbre
+ */
+
 $html .= '
+<style>
+	#toolbar { padding: 15px 6px; }
+</style>
 <div class="demo">
-
-
 	<span style="position:fixed;top:50px;px;z-index:100;opacity:0.4;" id="toolbar" class="ui-widget-header ui-corner-all">
-
-		<button id="beginning">go to beginning</button>
+		<!--button id="beginning">go to beginning</button>
 		<button id="rewind">rewind</button>
 		<button id="play">play</button>
 		<button id="stop">stop</button>
 		<button id="forward">fast forward</button>
-		<button id="end">go to end</button>
-
+		<button id="end">go to end</button-->
 		<input type="checkbox" id="shuffle" /><label for="shuffle">Shuffle</label>
-
 		<span id="repeat">
 			<input type="radio" id="slot0" name="repeat" checked="checked" /><label for="slot0">primary</label>
 			<input type="radio" id="slot1" name="repeat" /><label for="slot1">secondary</label>
@@ -119,12 +87,9 @@ $html .= '
 		</span>
 
 	</span>
-
 </div>
 ';
 
 $html .= '</body>';
-
 $html .= '</html>';
-
 echo $html;
